b7923430113233b618777cb53a6c9685d2ce63f9
anto <irianto223@gmail.com>
2017-09-14 16:31:09 +0700

    buat navbar frontend

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+    <ul>
+      <li><a href="#">Home</a></li>
+    </ul>
+    <ul>
+      <li><a href="#">Login</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
